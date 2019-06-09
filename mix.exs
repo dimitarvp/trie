74d46f5cbbff4f4deff986f573a5dd317cd3bc1a
Dimitar Panayotov <mitko.p@gmail.com>
2019-06-09 18:25:24 +0300

    Modernise dependencies and Elixir version, migrate way from `applications` legacy key and use `extra_applications` instead

--- mix.exs
+++ mix.exs
@@ -1,16 +1,23 @@
 defmodule Trie.Mixfile do
   use Mix.Project
 
+  @name "Trie"
+  @version "0.2.0"
+
   def project do
     [app: :trie,
-     version: "0.1.0",
-     elixir: "~> 1.3",
+     version: @version,
+     elixir: "~> 1.8",
+     name: @name,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     docs: [source_ref: "v#{@version}", main: "readme",
+            extras: ["README.md"]],
      deps: deps(),
 
+      # dependency: `excoveralls`-specific.
      test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test,
+     preferred_cli_env: [coveralls: :test,
                          "coveralls.detail": :test,
                          "coveralls.post": :test,
                          "coveralls.html": :test],
@@ -18,15 +25,15 @@ defmodule Trie.Mixfile do
   end
 
   def application do
-    [applications: [:logger]]
+    [extra_applications: [:logger]]
   end
 
   defp deps do
     [
-      {:benchwarmer, "~> 0.0.2", only: :dev},
-      {:ex_doc, "~> 0.14", only: :dev},
-      {:dialyxir, "~> 0.3", only: :dev},
-      {:excoveralls, "~> 0.5", only: :test}
+      {:benchee, "~> 1.0", only: :dev},
+      {:dialyxir, "~> 0.5", only: :dev},
+      {:ex_doc, "~> 0.20", only: :dev},
+      {:excoveralls, "~> 0.11", only: [:dev, :test]},
     ]
   end
 end
